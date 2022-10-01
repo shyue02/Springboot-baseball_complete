984204d23875e1b0c605ad2b279771f176060cf7
unknown <shyue02@naver.com>
2022-10-01 18:33:32 +0900

    DAO 메소드 파라메터 변수명 변경[수정]

--- src/main/java/site/metacoding/red/domain/player/PlayerDao.java
+++ src/main/java/site/metacoding/red/domain/player/PlayerDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface PlayerDao {
 	public List<Player> findAll(); 	// 찾아서 줘야하니까 리턴값이 필요하다 -> List<>
 	public Player findById(Integer id);	// Id로 받음
-	public void insert(Player stadium); //stadium으로 받음
-	public void update(Integer id, Player stadium);	// update는 보통 두 개 받는다
+	public void insert(Player player); //stadium으로 받음
+	public void update(Integer id, Player player);	// update는 보통 두 개 받는다
 	public void deleteById(Integer id); // Id로 삭제할꺼니까 ById
 }
--- src/main/java/site/metacoding/red/domain/team/TeamDao.java
+++ src/main/java/site/metacoding/red/domain/team/TeamDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface TeamDao {
 	public List<Team> findAll(); 	// 찾아서 줘야하니까 리턴값이 필요하다 -> List<>
 	public Team findById(Integer id);	// Id로 받음
-	public void insert(Team stadium); //stadium으로 받음
-	public void update(Integer id, Team stadium);	// update는 보통 두 개 받는다
+	public void insert(Team team); //stadium으로 받음
+	public void update(Integer id, Team team);	// update는 보통 두 개 받는다
 	public void deleteById(Integer id); // Id로 삭제할꺼니까 ById
 }
